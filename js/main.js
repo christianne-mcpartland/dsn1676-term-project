df67a3b4c68636932e3f453901053a9a43970e6b
Christiemcp <christianne.mcpartland@gmail.com>
2015-03-18 18:14:38 -0400

    creating the navigation toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 var $tabs = $('.tabs');
+var $panels = $('.panel');
+var $nav = $('nav');
+var $navBtn = $('.nav-button');
+
+$navBtn.on ('click', 'a', function () {
+    $nav.toggleClass ('nav-show');
+});
 
 $tabs.on('click', 'a', function (e) {
     e.preventDefault();
     var id = $(this).attr('href'); 
-    console.log(id);
+    
+    $panels.filter(':not([hidden])').attr('hidden', true);
+    $(id).removeAttr('hidden');
+    
+    $tabs.find('.js-current').removeClass('js-current');
+    $(this).addClass('js-current');
+    
+});
+
+var $btnExpand = $(".btn-expand");
+var $togglePanel = $('.toggle-panel');
+
+$btnExpand.on('click', function() {
+    $togglePanel.toggleClass('toggle-panel-expand');
 });
