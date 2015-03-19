ca443d21335d475009c7246c335515e9b5788392
Christiemcp <christianne.mcpartland@gmail.com>
2015-03-19 12:12:13 -0400

    Adding a new panel section
    -making the content hidden until clicked on
    -using the grid to align the image and text
    -styling the appearance of the panel

--- js/main.js
+++ js/main.js
@@ -25,3 +25,10 @@ var $togglePanel = $('.toggle-panel');
 $btnExpand.on('click', function() {
     $togglePanel.toggleClass('toggle-panel-expand');
 });
+
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+$btnCollapse.on('click', function () {
+    $panel.toggleClass('js-panel-collapse');
+});
