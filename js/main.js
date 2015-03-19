a23bbf1794aaa3ae233b2ccec8e33c6049c02eeb
Christiemcp <christianne.mcpartland@gmail.com>
2015-03-19 14:43:43 -0400

    Cleaning up the code
    -getting rid of the width
    -getting rid of the underline
    -fixing the margin on .panel
    -targeting the p in the panel instead of h2
    -reducing the padding

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-var $tabs = $('.tabs');
-var $panels = $('.panel');
-var $nav = $('nav');
-var $navBtn = $('.nav-button');
-
-$navBtn.on ('click', 'a', function () {
-    $nav.toggleClass ('nav-show');
-});
-
-$tabs.on('click', 'a', function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href'); 
-    
-    $panels.filter(':not([hidden])').attr('hidden', true);
-    $(id).removeAttr('hidden');
-    
-    $tabs.find('.js-current').removeClass('js-current');
-    $(this).addClass('js-current');
-    
-});
-
 var $btnExpand = $(".btn-expand");
 var $togglePanel = $('.toggle-panel');
 
@@ -32,3 +11,4 @@ var $panel = $('.panel');
 $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel-collapse');
 });
+
