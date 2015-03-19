f2ca5eeb1e69e36df682c2e2487454165e9e627d
Christiemcp <christianne.mcpartland@gmail.com>
2015-03-19 15:27:02 -0400

    Giving each panel it's own collapse / expand
    >Copying & pasting the javascript & css
    >Giving that code its own name for each panel, so that the panels
    expand/collapse individually

--- js/main.js
+++ js/main.js
@@ -5,9 +5,23 @@ $btnExpand.on('click', function() {
     $togglePanel.toggleClass('toggle-panel-expand');
 });
 
-var $btnCollapse = $('.btn-collapse-expand');
-var $panel = $('.panel');
+var $btnCollapseH = $('.btn-collapse-expand-h');
+var $panelH = $('.panel-h');
+
+$btnCollapseH.on('click', function () {
+    $panelH.toggleClass('js-panel-collapse-h');
+});
+
+var $btnCollapse = $('.btn-collapse-expand-d');
+var $panel = $('.panel-d');
 
 $btnCollapse.on('click', function () {
-    $panel.toggleClass('js-panel-collapse');
+    $panel.toggleClass('js-panel-collapse-d');
+});
+
+var $btnCollapseB = $('.btn-collapse-expand-b');
+var $panelB = $('.panel-b');
+
+$btnCollapseB.on('click', function () {
+    $panelB.toggleClass('js-panel-collapse-b');
 });
